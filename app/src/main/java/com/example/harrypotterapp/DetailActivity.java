043d5fc995842bb49ed1b05a139b6a338b054b03
JoseFuentesR <caca.candente@gmail.com>
2020-10-07 05:18:59 -0300

    "hecha la parte de detaill activity , falta un poco jugar con el diseño para que quede bien y editar los botones , tambien hice unos breves cambios a adapter character "

--- app/src/main/java/com/example/harrypotterapp/DetailActivity.java
+++ app/src/main/java/com/example/harrypotterapp/DetailActivity.java
@@ -3,12 +3,60 @@ package com.example.harrypotterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class DetailActivity extends AppCompatActivity {
 
+    String name,house,image,species,gender,date,patronus,howarts,actor,alive;
+    TextView txtname,txtspecies,txtgender,txthouse,txtbirth,txtpatronus,txthoward,txtactor,txtalive;
+    CircleImageView txtperfil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        name = getIntent().getStringExtra("NAME");
+        house = getIntent().getStringExtra("HOUSE");
+        image = getIntent().getStringExtra("IMAGE");
+        species = getIntent().getStringExtra("SPECIES");
+        gender = getIntent().getStringExtra("GENDER");
+        date = getIntent().getStringExtra("DATE");
+        patronus = getIntent().getStringExtra("PATRONUS");
+        howarts = getIntent().getStringExtra("HOWARTS");
+        actor = getIntent().getStringExtra("ACTOR");
+        alive = getIntent().getStringExtra("ALIVE");
+        //cargar los controles
+        txtname = findViewById(R.id.item_name);
+        txtspecies = findViewById(R.id.item_species);
+        txtgender = findViewById(R.id.item_gender);
+        txthouse = findViewById(R.id.item_house);
+        txtbirth = findViewById(R.id.item_date);
+        txtpatronus = findViewById(R.id.item_patronus);
+        txthoward = findViewById(R.id.item_howarts);
+        txtactor = findViewById(R.id.item_actor);
+        txtalive = findViewById(R.id.item_aliver);
+
+        txtperfil = findViewById(R.id.item_image);
+
+        txtname.setText(name);
+        txtspecies.setText(species);
+        txtgender.setText(gender);
+        txthouse.setText(house);
+        txtbirth.setText(date);
+        txtpatronus.setText(patronus);
+        txthoward.setText(howarts);
+        txtactor.setText(actor);
+        txtalive.setText(alive);
+
+        Glide.with(this).load(image).into(txtperfil);
+
+
+        Log.d("INFO",name+""+house+""+image+""+species+""+gender+""+date+""+patronus+""+howarts+""+actor+""+actor);
     }
 }
--- app/src/main/java/com/example/harrypotterapp/adapter/AdapterCharacter.java
+++ app/src/main/java/com/example/harrypotterapp/adapter/AdapterCharacter.java
@@ -44,8 +44,20 @@ public class AdapterCharacter extends RecyclerView.Adapter<AdapterCharacter.Char
 
         Character character = list.get(position);
         holder.item_name.setText(character.name);
+        //variables new detail
+        holder.item_gender.setText(character.gender);
+        holder.item_species.setText(character.species);
+        holder.item_date.setText(character.dateOfBirth);
+        holder.item_patronus.setText(character.patronus);
+        holder.item_howarts.setText(character.hogwartsStudent);
+        holder.item_actor.setText(character.actor);
+        holder.item_alive.setText(character.alive);
+        //end variables new
+
         Glide.with(activity).load(character.image).into(holder.item_image);
         holder.item_house.setText(character.house);
+        //esto es para que me pase el url de la imagen
+        holder.urlImage = character.image;
 
 
     }
@@ -59,7 +71,12 @@ public class AdapterCharacter extends RecyclerView.Adapter<AdapterCharacter.Char
 
         ImageView item_image;
         TextView item_name,item_house;
+        //var new
+        TextView item_species,item_gender,item_date,item_patronus,item_howarts,item_actor,item_alive ;
+        //
         RelativeLayout item_character;
+
+        String urlImage;
 
         public CharacterHolder(@NonNull View itemView) {
             super(itemView);
@@ -68,11 +85,31 @@ public class AdapterCharacter extends RecyclerView.Adapter<AdapterCharacter.Char
             item_image = itemView.findViewById(R.id.item_image);
             item_house = itemView.findViewById(R.id.item_house);
             item_character = itemView.findViewById(R.id.item_character);
+            //esto es por seguridad por si va por id la imagen
+            item_species = itemView.findViewById(R.id.item_species);
+            item_gender = itemView.findViewById(R.id.item_gender);
+            item_date = itemView.findViewById(R.id.item_date);
+            item_patronus = itemView.findViewById(R.id.item_patronus);
+            item_howarts = itemView.findViewById(R.id.item_howarts);
+            item_actor = itemView.findViewById(R.id.item_actor);
+            item_alive = itemView.findViewById(R.id.item_aliver);
 
             item_character.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Intent intent = new Intent(activity, DetailActivity.class);
+                    intent.putExtra("NAME",item_name.getText().toString());
+                    intent.putExtra("HOUSE",item_name.getText().toString());
+                    //
+                    intent.putExtra("IMAGE", urlImage);
+                    intent.putExtra("SPECIES",item_species.getText().toString());
+                    intent.putExtra("GENDER",item_gender.getText().toString());
+                    intent.putExtra("DATE",item_date.getText().toString());
+                    intent.putExtra("PATRONUS",item_patronus.getText().toString());
+                    intent.putExtra("HOWARTS",item_howarts.getText().toString());
+                    intent.putExtra("ACTOR",item_actor.getText().toString());
+                    intent.putExtra("ALIVE",item_alive.getText().toString());
+                    //
                     activity.startActivity(intent);
                 }
             });
